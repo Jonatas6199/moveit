b5dbc76b21edbb50478c6da72e8c16cabbfddf71
Jonatas6199 <jojokl@hotmail.com>
2021-02-28 01:26:10 -0300

    include initial components into app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,21 @@
 import React from 'react';
-import './App.css';
+import {Button} from './components/button'
 
 //App é um componente do react que cria conteúdo em HTML
 function App() {
   return (
-        <h1>HELLO NLW</h1>
+    <div>
+      <Button color="red">
+        Botão 1
+      </Button>
+      <Button color="green">
+        Botão 2
+      </Button>
+      <Button color="blue">
+        Botão 3
+      </Button>
+    </div>
+
   );
 }
 
