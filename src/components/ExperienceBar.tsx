c1e8c1a57ba93d565b71e0fdcc00f6a8c86dcb94
Jonatas6199 <jojokl@hotmail.com>
2021-03-07 21:21:33 -0300

    add children components into experienceBar

--- src/components/ExperienceBar.tsx
+++ src/components/ExperienceBar.tsx
@@ -2,7 +2,13 @@ export function ExperienceBar(){
   return(
     <header className="experience-bar">
         <span>0 xp</span>
-        <div/>
+        <div>
+          <div style={{ width:'50%' }}/>
+          
+          <span style={{left: '50%'}} className="current-experience">
+            300xp
+          </span>
+        </div>
         <span>600 xp</span>
     </header>
   )
