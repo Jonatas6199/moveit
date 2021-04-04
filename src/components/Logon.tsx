cf24ed0106d5453fa8dcf3398330d52a7fa3cc26
Jonatas6199 <jojokl@hotmail.com>
2021-04-04 02:45:44 -0300

    Add login tsx and css files

---
+++ src/components/Logon.tsx
@@ -0,0 +1,31 @@
+import styles from "../styles/components/Logon.module.css";
+import { useRouter } from "next/router";
+export function Logon(){
+
+    const router = useRouter();
+    function homePage(){
+       router.push("/");
+    }
+    return (
+        <div className={styles.container}>
+            <img src="icons/moveit-logo.svg" alt="logo"></img>
+            <p>
+                Bem-vindo
+            </p>
+            <div className={styles.github}>
+                <img src="icons/github.svg" alt="twitter"></img>
+                <p>Faça seu login com seu Github <br></br> para começar
+                </p>
+            </div>
+            
+            <div className={styles.logar}>
+                <input type="text"></input>
+                <button onClick={homePage}>
+                    <img src="icons/arrow.svg" alt="Logar"></img>
+                </button>
+            </div>
+           
+           
+        </div>
+    )
+}
