0c38a274b1d39e30aed429feb2326fe5f3e5ecad
Jonatas6199 <jojokl@hotmail.com>
2021-04-04 22:20:56 -0300

    add  ChallengesProvider on index tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,5 @@
 import Head from "next/head"
-
+import { GetServerSideProps } from "next";
 import { CompletedChallenges } from "../components/CompletedChallenges";
 import { Countdown } from "../components/Countdown";
 import { ExperienceBar } from "../components/ExperienceBar";
@@ -9,28 +9,58 @@ import { ChallengeBox } from "../components/ChallengeBox";
 
 import styles from "../styles/pages/Home.module.css";
 import { CountdownProvider } from "../contexts/CountdownContext";
-export default function Home() {
-  return (
-    <div className={styles.container}>
-      <Head>
-        <title>
-          Inicio | move.it
-        </title>
-      </Head>
-      <ExperienceBar/>
+import { ChallengesProvider } from "../contexts/ChallengesContext";
 
-    <CountdownProvider>
-      <section>
-        <div>
-          <Profile/>
-          <CompletedChallenges/>
-          <Countdown/>
-        </div>
-        <div>
-         <ChallengeBox/>
-        </div>
-      </section>
-    </CountdownProvider>
-  </div>
+interface HomeProps{
+  level: number;
+  currentExperience: number;
+  challengesCompleted: number;
+}
+export default function Home(props: HomeProps) {
+  return (
+    <ChallengesProvider
+      level = {props.level}
+      currentExperience = {props.currentExperience}
+      challengesCompleted = {props.challengesCompleted} 
+    >
+      <div className={styles.container}>
+        <Head>
+          <title>
+            Inicio | move.it
+        </title>
+        </Head>
+        <ExperienceBar />
+
+        <CountdownProvider>
+          <section>
+            <div>
+              <Profile />
+              <CompletedChallenges />
+              <Countdown />
+            </div>
+            <div>
+              <ChallengeBox />
+            </div>
+          </section>
+        </CountdownProvider>
+      </div>
+    </ChallengesProvider>
   )
+}
+
+//antes de construir a interface vai chamar isso aqui, e vai passar os dados já prontos
+//pro componente antes de renderizar
+
+//tudo isso aqui é executado no servidor node/nextjs e não no browser
+export const getServerSideProps: GetServerSideProps = async (ctx) => {
+
+  const { level, currentExperience, challengesCompleted } = ctx.req.cookies;
+
+  return {
+    props: {
+      level: Number(level),
+      currentExperience: Number(currentExperience),
+      challengesCompleted: Number(challengesCompleted)
+    }
+  }
 }
